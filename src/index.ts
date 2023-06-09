96a2ce2ad32c868cda44afcd380378ef2bca7c78
Hibiki Sakuraoka <wbydo@users.noreply.github.com>
2023-06-09 11:46:27 +0900

    restart

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello World!!');
+import { setTimeout } from 'node:timers/promises';
+
+const main = async () => {
+  for (const i of [1, 2, 3, 4, 5]) {
+    await setTimeout(500);
+    console.log(`Hello World ${i}`);
+  }
+};
+
+main().catch((err) => console.error(err));

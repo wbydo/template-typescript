98fe0bd6c6c9a73f19c32b52617b31e6b747e248
Hibiki Sakuraoka <wbydo@users.noreply.github.com>
2025-01-04 18:05:14 +0900

    Update eslint and prettier configurations

--- eslint.config.js
+++ eslint.config.js
@@ -3,16 +3,16 @@
  * https://zenn.dev/kazukix/articles/eslint-config-2024-09
  */
 
-import tseslint from "typescript-eslint";
-import prettierConfig from "eslint-config-prettier";
-import eslint from "@eslint/js";
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import eslint from '@eslint/js';
 
 export default tseslint.config(
   {
     // このオブジェクトは ignores プロパティだけにする必要あり
-    ignores: ["dist"], // ESLint のチェック対象外 (node_modules と .git はデフォルトで対象外)
+    ignores: ['dist'], // ESLint のチェック対象外 (node_modules と .git はデフォルトで対象外)
   },
   eslint.configs.recommended,
   ...tseslint.configs.strict,
-  prettierConfig
+  prettierConfig,
 );
